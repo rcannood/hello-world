312da06f2a90521d20556a5908fb658179491558
vsoch <vsochat@stanford.edu>
2016-10-25 18:58:32 -0700

    adding run script to install singularity-python and (possibly) build image from spec

---
+++ run.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python
+
+import os
+import tempfile
+from singularity.package import build_from_spec
+
+tmpdir = tempfile.mkdtemp()
+singularityFile = "Singularity"
+sudopw = ''
+name = 'test'
+build_dir = os.getcwd()
+size=None
+
+if os.path.exists(singularityFile):
+
+    # Build an image package from the specfile
+    image_package = build_from_spec(spec=singularityFile,
+                                    name=name,
+                                    build_dir=tmpdir,
+                                    size=size,
+                                    sudopw=sudopw)
+
+print(image_package)
+#TODO: need to be able to return an error/build failed to user
